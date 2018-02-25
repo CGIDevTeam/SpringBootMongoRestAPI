1b20c925878d67a660ef18f0bfb9749d9d2d63f6
Julien JUGE <jjuge.julien@gmail.com>
2018-02-25 17:39:02 +0100

    Add Swagger for /context & /application

---
+++ src/main/java/dev/julien/juge/utils/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package dev.julien.juge.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("dev.julien.juge.controllers"))
+                .paths(regex("/application.*|/context.*"))
+                .build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Spring Boot Mongo Rest API",
+                "API Rest with Mongo database.",
+                "1.0.0",
+                "Terms of service",
+                new Contact("Julien JUGE", "http://cgi.com", "jjuge.julien@gmail.com"),
+                "License of API", "API license URL", Collections.emptyList());
+    }
+}
